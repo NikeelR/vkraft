035b6596d764a2936dbfbc21e7461adda1cdeaa8
Nikeel <79253423+NikeelR@users.noreply.github.com>
2022-12-27 12:55:11 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import "./App.css";
-import "./styles.scss";
+import "./styles.css";
 import vector10 from "./assets/vector10.svg";
 import vector7 from "./assets/vector7.svg";
 import vector2 from "./assets/vector2.svg";
